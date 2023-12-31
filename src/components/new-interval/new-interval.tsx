ad0299c9fa890e415ec8dc3ed21dc7699f2eef76
RatLad69 <quirinus613@gmail.com>
2023-12-30 23:51:53 -0600

    Moved inToHMS to timeCalcs

--- src/components/new-interval/new-interval.tsx
+++ src/components/new-interval/new-interval.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import styles from './new-interval.module.scss';
 import { useState } from 'react';
+import { inToHMS } from '../../timeCalcs'
 
 export interface NewIntervalProps {
     className?: string;
@@ -16,31 +17,6 @@ let HMSduration = [0, 0, 0, 0];
  */
 export const NewInterval = ({ className, addNewAlarm }: NewIntervalProps) => {
 
-    const inToHMS = (uInput: string) => {
-        const hmsList = uInput.split(":");
-        let [hours, minutes, seconds] = [0, 0, 0];
-        if (hmsList.length === 1) {
-            seconds = Number(hmsList[0]);
-        } else if (hmsList.length === 2) {
-            minutes = Number(hmsList[0]);
-            seconds = Number(hmsList[1]);
-        } else if (hmsList.length === 3) {
-            hours = Number(hmsList[0]);
-            minutes = Number(hmsList[1]);
-            seconds = Number(hmsList[2]);
-        }
-        let totalSeconds = 60 * 60 * hours + 60 * minutes + seconds;
-        if (seconds >= 60) {
-            minutes += Math.floor(seconds/60);
-            seconds -= 60 * Math.floor(seconds/60);
-        }
-        if (minutes >= 60) {
-            hours += Math.floor(minutes/60);
-            minutes -= 60 * Math.floor(minutes/60);
-        }
-        return [hours, minutes, seconds, totalSeconds];
-    }
-
     const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
         const value = (event.target as HTMLInputElement).value;
         HMSduration = inToHMS(value);
@@ -50,7 +26,6 @@ export const NewInterval = ({ className, addNewAlarm }: NewIntervalProps) => {
     const handleClick = () => {
         console.log("click");
         addNewAlarm(HMSduration);
-
     }
 
     return <div className={classNames(styles.root, className)}>
--- src/timeCalcs.ts
+++ src/timeCalcs.ts
@@ -15,6 +15,31 @@ export const HMStoMS = (HMS: Array<number>) => {
     return ms;
 }
 
+export const inToHMS = (uInput: string) => {
+    const hmsList = uInput.split(":");
+    let [hours, minutes, seconds] = [0, 0, 0];
+    if (hmsList.length === 1) {
+        seconds = Number(hmsList[0]);
+    } else if (hmsList.length === 2) {
+        minutes = Number(hmsList[0]);
+        seconds = Number(hmsList[1]);
+    } else if (hmsList.length === 3) {
+        hours = Number(hmsList[0]);
+        minutes = Number(hmsList[1]);
+        seconds = Number(hmsList[2]);
+    }
+    let totalSeconds = 60 * 60 * hours + 60 * minutes + seconds;
+    if (seconds >= 60) {
+        minutes += Math.floor(seconds/60);
+        seconds -= 60 * Math.floor(seconds/60);
+    }
+    if (minutes >= 60) {
+        hours += Math.floor(minutes/60);
+        minutes -= 60 * Math.floor(minutes/60);
+    }
+    return [hours, minutes, seconds, totalSeconds];
+}
+
 export const timeToString = (time: Array<number>) => {
     let timeString = "";
     for (let i = 0; i < time.length - 1; i++) {
